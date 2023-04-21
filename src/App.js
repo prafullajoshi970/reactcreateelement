c14ec3cb2f76d5af1348d8bd37e8b0699c2b2842
prafullajoshi970 <97543959+prafullajoshi970@users.noreply.github.com>
2023-04-21 07:45:21 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const element1 = React.createElement('h1', {className: 'title'}, 'Topic Coverd');
+  const element2 = React.createElement('p', {className: 'description'}, 'The following is a lst of all topics cover in the MDN learing area.');
+  const element3 = React.createElement('a',{className:'anc'},'Getting started with the web')
+  const element4= React.createElement('p',{className:'description'},'Provide a perticular introduction to web development for complete beinners.')
+  const element5 = React.createElement('a',{className:'anc'},'HTML-Structuring the web')
+  const element6= React.createElement('p',{className:'description'},'Html is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.')
+  const element7 = React.createElement('a',{className:'anc'},'CSS-styling the the web')
+  const element8= React.createElement('p',{className:'description'},'CSS is language that we use to control our web content style and layout ,as well as adding behaviour like animation .This provides comprehensive covrage of CSS .')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      {element1}
+      {element2}
+      {element3}
+      {element4}
+      {element5}
+      {element6}
+      {element7}
+      {element8}
     </div>
   );
-}
+};
 
 export default App;
